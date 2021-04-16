a9948e49be963c6ea10d87d22576344463ec7951
tqch <tqch2020@gmail.com>
2021-04-16 18:15:12 -0400

    add 20 newsgroup dataset and reduce the vocabulary size

--- src/utils/data.py
+++ src/utils/data.py
@@ -6,7 +6,7 @@ import logging
 import nltk
 from nltk.tokenize import word_tokenize
 from nltk.stem.wordnet import WordNetLemmatizer
-from gensim.corpora import Dictionary,MmCorpus
+from gensim.corpora import Dictionary, MmCorpus
 
 nltk.download('wordnet')
 logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
@@ -14,25 +14,32 @@ logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=lo
 ROOT = os.path.expanduser("~/dpfa-imdb")
 DATA_DIR = os.path.join(ROOT, "datasets")
 IMDB_SOURCE = "https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz"
+NEWSGROUP_SOURCE = "http://qwone.com/~jason/20Newsgroups/20news-18828.tar.gz"
 PATTERN = re.compile(r"\d+_([0-9]|10)\.txt")
 LEMMATIZER = WordNetLemmatizer()
 IMDB_DECOMPRESS_FOLDER = "aclImdb"
+NEWSGROUP_DECOMPRESS_FOLDER = "20news-18828"
 
 
 def pull_from_url(path, dataset="imdb"):
     if dataset == "imdb":
-        filename = IMDB_SOURCE.split("/")[-1]
-        if not os.path.exists(os.path.join(path,filename)):
-            logging.info("Pulling IMDB dataset from URL: {}".format(IMDB_SOURCE))
-            download(IMDB_SOURCE, out=path)
-        else:
-            logging.info("File aleady exists, skip download!")
-        if not os.path.exists(os.path.join(path,IMDB_DECOMPRESS_FOLDER)):
-            with tarfile.open(os.path.join(path, filename), "r:gz") as gz:
-                logging.info("Extracting data from compressed file...".format(IMDB_SOURCE))
-                gz.extractall(path=path)
-        else:
-            logging.info("Compressed file was already extracted in current folder!")
+        url = IMDB_SOURCE
+        decompress_folder = IMDB_DECOMPRESS_FOLDER
+    if dataset == "20newsgroup":
+        url = NEWSGROUP_SOURCE
+        decompress_folder = NEWSGROUP_DECOMPRESS_FOLDER
+    filename = url.split("/")[-1]
+    if not os.path.exists(os.path.join(path, filename)):
+        logging.info("Pulling {} dataset from URL: {}".format(dataset.upper(), url))
+        download(url, out=path)
+    else:
+        logging.info("File aleady exists, skip download!")
+    if not os.path.exists(os.path.join(path, decompress_folder)):
+        with tarfile.open(os.path.join(path, filename), "r:gz") as gz:
+            logging.info("Extracting data from compressed file...")
+            gz.extractall(path=path)
+    else:
+        logging.info("Compressed file was already extracted in current folder!")
 
 
 def load_imdb(
@@ -50,49 +57,86 @@ def load_imdb(
         docs = []
         ratings = []
         labels = []
-        for label in ["pos", "neg"]:
-            work_dir = os.path.join(data_folder, IMDB_DECOMPRESS_FOLDER, subfolder, label)
+        for cls in ["pos", "neg"]:
+            work_dir = os.path.join(data_folder, IMDB_DECOMPRESS_FOLDER, subfolder, cls)
             filenames = os.listdir(work_dir)
             for file in filenames:
                 with open(os.path.join(work_dir, file), "r", encoding="utf8") as f:
                     docs.append(word_tokenize(f.read().strip()))
                     ratings.append(PATTERN.search(file).group(1))
-                    labels.append(1)
+                    labels.append(1 if cls == "pos" else 0)
         docs = [[LEMMATIZER.lemmatize(token) for token in doc] for doc in docs]
         if not os.path.exists(processed_folder):
             os.mkdir(processed_folder)
         if train:
             logging.info("Building dictionary and BOW corpus for training data...")
             corpus, dictionary = build_corpus(docs)
-            MmCorpus.serialize(os.path.join(processed_folder, "train_corpus.mm"),corpus)
+            MmCorpus.serialize(os.path.join(processed_folder, "train_corpus.mm"), corpus)
             dictionary.save(os.path.join(processed_folder, "dictionary"))
-            return corpus, dictionary
         else:
             logging.info("Building BOW corpus for testing data...")
             dictionary = Dictionary.load(os.path.join(processed_folder, "dictionary"))
             corpus, _ = build_corpus(docs, dictionary)
             MmCorpus.serialize(os.path.join(processed_folder, "test_corpus.mm"), corpus)
-            return corpus, dictionary
+        return corpus, dictionary
     else:
         try:
             if train:
-                return MmCorpus(os.path.join(processed_folder, "train_corpus.mm")),\
-                       Dictionary.load(os.path.join(processed_folder, "dictionary"))
+                return MmCorpus(os.path.join(processed_folder, "train_corpus.mm")), \
+                       Dictionary.load(os.path.join(processed_folder, "dictionary")),
             else:
-                return MmCorpus(os.path.join(processed_folder, "test_corpus.mm")),\
+                return MmCorpus(os.path.join(processed_folder, "test_corpus.mm")), \
                        Dictionary.load(os.path.join(processed_folder, "dictionary"))
         except FileNotFoundError:
             logging.warning("The dataset does not exist, please set download to True!")
-            return None,None
+            return None, None
+
+
+def load_20newsgroup(
+        root=DATA_DIR,
+        download=False
+):
+    data_folder = os.path.join(root, "datasets/20newsgroup")
+    processed_folder = os.path.join(data_folder, "processed")
+    if download:
+        if not os.path.exists(data_folder):
+            os.makedirs(data_folder)
+        pull_from_url(data_folder, dataset="20newsgroup")
+        docs = []
+        groups = []
+        subfolders = os.listdir(os.path.join(data_folder, NEWSGROUP_DECOMPRESS_FOLDER))
+        for subf in subfolders:
+            work_dir = os.path.join(data_folder, NEWSGROUP_DECOMPRESS_FOLDER, subf)
+            filenames = os.listdir(work_dir)
+            for file in filenames:
+                with open(os.path.join(work_dir, file), "r") as f:
+                    docs.append(word_tokenize(f.read().strip()))
+                    groups.append(subf)
+        docs = [[LEMMATIZER.lemmatize(token) for token in doc] for doc in docs]
+        if not os.path.exists(processed_folder):
+            os.mkdir(processed_folder)
+        logging.info("Building dictionary and BOW corpus for the dataset...")
+        corpus, dictionary = build_corpus(docs)
+        MmCorpus.serialize(os.path.join(processed_folder, "corpus.mm"), corpus)
+        dictionary.save(os.path.join(processed_folder, "dictionary"))
+        return corpus, dictionary
+    else:
+        try:
+            return MmCorpus(os.path.join(processed_folder, "corpus.mm")), \
+                   Dictionary.load(os.path.join(processed_folder, "dictionary"))
+        except FileNotFoundError:
+            logging.warning("The dataset does not exist, please set download to True!")
+            return None, None
 
 
 def build_corpus(docs, dictionary=None):
     if dictionary is None:
         dictionary = Dictionary(docs)
-        dictionary.filter_extremes()
+        dictionary.filter_extremes(no_below=50, no_above=0.3)
     corpus = [dictionary.doc2bow(doc) for doc in docs]
     return corpus, dictionary
 
 
 if __name__ == "__main__":
     load_imdb(root="./", download=True)
+    load_20newsgroup(root="./", download=True)
