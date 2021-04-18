35c1fa375935545986fc7ae5db2f54b47cb2b1c4
tqchen <tqch2020@gmail.com>
2021-04-18 17:12:56 -0400

    return labels

--- data_prep.py
+++ data_prep.py
@@ -4,6 +4,7 @@ from smart_open import open
 from sklearn.feature_extraction.text import CountVectorizer
 from nltk.tokenize import word_tokenize, RegexpTokenizer
 from scipy.sparse import save_npz
+import numpy as np
 
 import nltk
 
@@ -15,17 +16,15 @@ from nltk.corpus import stopwords
 IMDB_SOURCE = 'https://ai.stanford.edu/~amaas/data/sentiment/aclImdb_v1.tar.gz'
 PATTERN = re.compile(r'aclImdb/(train|test)/(pos|neg)/\d+_([0-9]|10)\.txt')
 
-labels = []
-
 
 def generate_docs_from_url(url=IMDB_SOURCE, pat=PATTERN):
     with open(url, 'rb') as infile:
         with tarfile.open(fileobj=infile) as tar:
             for member in tar.getmembers():
-                if member.isfile() and re.match(r'aclImdb/(train|test)/(pos|neg)/\d+_([0-9]|10)\.txt', member.name):
+                if member.isfile() and pat.match(member.name):
                     member_bytes = tar.extractfile(member).read()
-                    labels.append(pat.search(member.name).group(2))
-                    yield member_bytes.decode('utf8', errors='replace')
+                    label = int(pat.search(member.name).group(2) == 'pos')
+                    yield member_bytes.decode('utf8', errors='replace'), label
 
 
 STOPS = set(stopwords.words('english'))
@@ -55,25 +54,21 @@ def prepare_sparse_matrix(n_train, n_test, max_vocab_size, max_df=.5, min_df=10,
         min_df=min_df,
         max_features=max_vocab_size,
     )
-
-    docs = list(generate_docs_from_url())[:(n_train + n_test)]
-    docs_tr = tf_vectorizer.fit_transform(docs[:n_train])
-    docs_te = tf_vectorizer.transform(docs[n_train:])
-    return tf_vectorizer, docs_tr, docs_te
+    docs = []
+    labels = []
+    for doc, lab in generate_docs_from_url():
+        docs.append(doc)
+        labels.append(lab)
+    np.random.seed(1234)
+    indices = np.random.choice(len(docs), size=n_train + n_test, replace=False)
+    docs_tr = [docs[idx] for idx in indices[:n_train]]
+    docs_te = [docs[idx] for idx in indices[n_train:]]
+    labels_tr = np.array([labels[idx] for idx in indices[:n_train]])
+    labels_te = np.array([labels[idx] for idx in indices[n_train:]])
+    docs_tr = tf_vectorizer.fit_transform(docs_tr)
+    docs_te = tf_vectorizer.transform(docs_te)
+    return tf_vectorizer, docs_tr, docs_te, labels_tr, labels_te
 
 
 if __name__ == '__main__':
-    vectorizer = CountVectorizer(
-        tokenizer=word_tokenize,
-        stop_words='english',
-        max_df=0.3,
-        min_df=5
-    )
-
-    bow = vectorizer.fit_transform(list(generate_docs_from_url())) # (50000, 41545)
-    labels = list(map(lambda x: '1\n' if x == 'pos' else '0\n', labels))
-    with open("./labels.txt", "w") as f:
-        f.writelines(labels)
-    save_npz('./imdb_bow.npz', bow)
-    with open('./vectorizer.pkl', 'wb') as f:
-        pickle.dump(vectorizer, f)
+    prepare_sparse_matrix(800, 200, 3000)
